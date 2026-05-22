bacdfdd0b0e91a1e5cf6b4d1c170b2e52f5b5ba6
lyh543 <lyh543@outlook.com>
2026-05-22 15:42:16 +0800

    ci: inject upstream nightly version metadata

--- packages/bruno-electron/electron-builder-config.ci.js
+++ packages/bruno-electron/electron-builder-config.ci.js
@@ -1,7 +1,16 @@
 const baseConfig = require('./electron-builder-config');
 
+const nightlyVersion = process.env.BRUNO_NIGHTLY_VERSION;
+
 module.exports = {
   ...baseConfig,
+  buildVersion: nightlyVersion || baseConfig.buildVersion,
+  extraMetadata: nightlyVersion
+    ? {
+        ...(baseConfig.extraMetadata || {}),
+        version: nightlyVersion
+      }
+    : baseConfig.extraMetadata,
   afterSign: undefined,
   mac: {
     ...baseConfig.mac,
